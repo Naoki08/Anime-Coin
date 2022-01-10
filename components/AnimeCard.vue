eab6008e555540999d648adeb7272d5644bb5301
Naoki <36870844+Naoki08@users.noreply.github.com>
2022-01-10 16:29:54 +0900

    アニメコインのカウンターを実装

--- components/AnimeCard.vue
+++ components/AnimeCard.vue
@@ -6,6 +6,30 @@
     <v-card-title>
       {{ info.title }}
     </v-card-title>
+
+    <v-btn
+      class="mx-2"
+      fab
+      color="indigo"
+      @click="decrementCount()"
+    >
+      <v-icon dark>
+        mdi-minus
+      </v-icon>
+    </v-btn>
+    {{ count }}
+    <v-btn
+      class="mx-2"
+      fab
+      color="indigo"
+      absolute
+      right
+      @click="incrementCount()"
+    >
+      <v-icon dark>
+        mdi-plus
+      </v-icon>
+    </v-btn>
   </v-card>
 </template>
 
@@ -20,5 +44,19 @@ export default Vue.extend({
       required: true
     }
   },
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    incrementCount(): void {
+      ++this.count;
+    },
+    decrementCount(): void {
+      if(this.count == 0) return;
+      --this.count;
+    }
+  }
 })
 </script>
